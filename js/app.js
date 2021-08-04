5355ed701d9f69ea11d1c35a2299dc2455cdcdd9
Gjerden <marie.gjerden@nordicsemi.no>
2021-08-04 12:59:45 +0200

    separate charts

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 // Global objects
 const api = new NRFCloudAPI("af2cf1f0a9b60f858a98dc956ce7c98a3800857b");
-const deviceId = "352656106106472";
 let counterInterval;
 let requestInterval;
 let temp;
 let humid;
 let index;
-console.log(index);
+let fake_weight;
+
 
 // Collection of update functions for different message types of nRFCloud device messages
 const updateFunc = {
@@ -22,6 +22,12 @@ const updateFunc = {
 		$('#humidity').text(data);
 		humid = parseFloat(data);
 		
+	},
+	RSRP: data => {
+		var f_data = parseFloat(data).toFixed(2);
+		data = f_data.toString();
+		$('#weight').text(data);
+		fake_weight = parseFloat(data);
 	}
 }
 
@@ -29,19 +35,28 @@ const updateFunc = {
 const updateTime = {
 	TEMP: time => {
 		var currentDate = new Date(time);
-		index = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDay()+1, currentDate.getHours(), currentDate.getMinutes(), currentDate.getSeconds());
+		index = new Date(currentDate.getFullYear(), currentDate.getMonth(), 
+						currentDate.getDay()+1, currentDate.getHours(), currentDate.getMinutes(), 
+						currentDate.getSeconds());
 	},
 	HUMID: time => {
 		var currentDate = new Date(time);
-		console.log(time*1000);
-		console.log(currentDate);
-		index = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDay()+1, currentDate.getHours(), currentDate.getMinutes(), currentDate.getSeconds());
-		console.log(index);
-
+		index = new Date(currentDate.getFullYear(), currentDate.getMonth(), 
+						currentDate.getDay()+1, currentDate.getHours(), currentDate.getMinutes(), 
+						currentDate.getSeconds());
+	},
+	RSRP: time => {
+		var currentDate = new Date(time);
+		index = new Date(currentDate.getFullYear(), currentDate.getMonth(), 
+						currentDate.getDay()+1, currentDate.getHours(), currentDate.getMinutes(), 
+						currentDate.getSeconds());
 	}
 }
 
-function checkNRFCloudMessages(new_data, chart, options) {
+
+function checkNRFCloudMessages(temp_data, t_chart, t_options,
+							   humid_data, h_chart, h_options,
+							   weight_data, w_chart, w_options) {
 
 	// check nRFCloud messages from the device every 5 seconds
 	requestInterval = setInterval(async () => {
@@ -57,21 +72,19 @@ function checkNRFCloudMessages(new_data, chart, options) {
 			}
 			updateFunc[appId](data);
 			updateTime[appId](time);
-			new_data.addRow([index, temp, humid]);
-			chart.draw(new_data, options);
+			// update temperature chart
+			temp_data.addRow([index, temp]);
+			t_chart.draw(temp_data, t_options);
+			// update humidity chart
+			humid_data.addRow([index, humid]);
+			h_chart.draw(humid_data, h_options);
+			// update weight chart
+			weight_data.addRow([index, fake_weight]);
+			w_chart.draw(weight_data, w_options);
 		});
 	}, 5000);
 }
 
-
-
-//new chart
-function drawLast24Hours(){
-		// function that is run one time when the website is loaded; will draw stored data
-		// from the last 24 hours where the last stored data is initialized as current time.
-}
-
-//new chart
 
 google.charts.load("current", {
 	packages: ["corechart", "line"]
@@ -81,70 +94,95 @@ google.charts.load("current", {
   google.charts.setOnLoadCallback(drawChart);
   
   function drawChart() {
-	// create data object with default value
-	var new_data = new google.visualization.DataTable();
+	// create temp data object with default value
+	var temp_data = new google.visualization.DataTable();
 	let initialDate = new Date();
-	new_data.addColumn("datetime","Time");
-	new_data.addColumn("number","Temperature");
-	new_data.addColumn("number","Humidity");
-	new_data.addRow([new Date(initialDate.getFullYear(),initialDate.getMonth(), initialDate.getDay()+1, initialDate.getHours(), initialDate.getMinutes(), initialDate.getSeconds()), NaN, NaN]);
-
+	temp_data.addColumn("datetime","Time");
+	temp_data.addColumn("number","Temperature");
+	temp_data.addRow([new Date(initialDate.getFullYear(),initialDate.getMonth(), 
+					 initialDate.getDay()+1, initialDate.getHours(), initialDate.getMinutes(), 
+					 initialDate.getSeconds()), NaN]);
+	
+	// create humid data object with default value
+	var humid_data = new google.visualization.DataTable();
+	humid_data.addColumn("datetime","Time");
+	humid_data.addColumn("number","Humidity");
+	humid_data.addRow([new Date(initialDate.getFullYear(),initialDate.getMonth(), 
+					  initialDate.getDay()+1, initialDate.getHours(), initialDate.getMinutes(), 
+					  initialDate.getSeconds()), NaN]);
+	
+	// create data object for weight graph
+	var weight_data = new google.visualization.DataTable();
+	weight_data.addColumn("datetime","Time");
+	weight_data.addColumn("number","Weight");
+	weight_data.addRow([new Date(initialDate.getFullYear(),initialDate.getMonth(), 
+					   initialDate.getDay()+1, initialDate.getHours(), initialDate.getMinutes(), 
+					   initialDate.getSeconds()), NaN]);
+	
 	// create options object with titles, colors, etc.
-	let options = {
-	  title: "Temperature and Humidity",
+	let t_options = {
+		title: "Temperature",
 	  hAxis: {
 		title: "Time"
 	  },
-	  curveType: "function",
-	  series: {
-		0: {targetAxisIndex: 0},
-		1: {targetAxisIndex: 1}
-	  },
-	  vAxes: {
-		// Adds titles to each axis.
-		0: {title: 'Temps (Celsius)'},
-		1: {title: 'Humidity (%)'}
+	  vAxis: {
+		viewWindow: {
+		  max: 45,
+		  min: 0
+		},
+		title: "Temperature"
+	  }
+	};
+	// options for humidity plot
+	let h_options = {
+		title: "Humidity",
+	  hAxis: {
+		title: "Time"
 	  },
 	  vAxis: {
 		viewWindow: {
 		  max: 50
-		}
-	  }
+		},
+		title: "Humidity"
+	  },
+	  colors: ["#a52714"]
 	};
+	// create options for weight object with titles etc.
+	let w_options = {
+		title: "Weight",
+		hAxis: {
+		  title: "Time"
+		},
+		vAxis: {
+		  viewWindow: {
+			max: 50
+		  },
+		  title: "Weight (kg)"
+		},
+		colors: ["#ff9a00"]
+	  };
 
-	// draw chart on load
-	let chart = new google.visualization.LineChart(
-	  document.getElementById("chart_hum")
+	// draw the two charts on load
+	let t_chart = new google.visualization.LineChart(
+	  document.getElementById("chart_temp")
 	);
-	chart.draw(new_data, options);
-	checkNRFCloudMessages(new_data, chart, options);
-	setInterval(async() =>{
- 	// instead of this random, you can make an ajax call for the current cpu usage or what ever data you want to display
- 	
-		const { items } = await api.getMessages(localStorage.getItem('deviceId') || '');
+	let w_chart = new google.visualization.LineChart(
+		document.getElementById("chart_weight")
+	);
+	let h_chart = new google.visualization.LineChart(
+		document.getElementById("chart_hum")
+	);
 
-		(items || [])
-		.map(({ message }) => message)
-		.forEach(({ appId, data, time }) => {
-			if (!updateFunc[appId]) {
-				console.log('unhandled appid', appId, data);
-				return;
-			}
-			updateFunc[appId](data);
-			updateTime[appId](time);
-		});
-		new_data.addRow([index, temp, humid]);
-		chart.draw(new_data, options);
-	 // update current time index
-	/*currentDate = new Date();
-  	index = [currentDate.getHours(), currentDate.getMinutes(), currentDate.getSeconds(), currentDate.getMilliseconds()];
-	*/
-	}, 5000);
+	t_chart.draw(temp_data, t_options);
+	w_chart.draw(weight_data, w_options);
+	h_chart.draw(humid_data, h_options);
 
-  }
+	//update messages
+	checkNRFCloudMessages(temp_data, t_chart, t_options, 
+					 	  humid_data, h_chart, h_options, 
+						  weight_data, w_chart, w_options);
 
-  //end new chart
-
+}
 
 
 // Main function
@@ -158,5 +196,4 @@ $(document).ready(() => {
 		localStorage.setItem('apiKey', api.accessToken);
 		loadDeviceNames();
 	});
-	//checkNRFCloudMessages(new_data,chart,options);
-	});
+});
