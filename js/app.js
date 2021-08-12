0431d03b5b288b58ee3133ee710c60c5eab2f0a0
Maria Gilje <marigil@stud.ntnu.no>
2021-08-12 14:16:50 +0200

    data from both hives in same plot

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
 // Global objects
-const api = new NRFCloudAPI("f41964799625f69d7c32ca15040b251f2b88a6e6"); // Erik
-const roofId = "nrf-352656106119046"; // Roof
-const thingyId = "nrf-352656106101531"; // Thingy31
-const deviceId = thingyId;
 let counterInterval;
 let requestInterval;
 let temp;
+let temp_arr = [];
 let humid;
+let humid_arr = [];
 let weight;
 let index;
 let b_in;
@@ -86,7 +84,7 @@ function checkNRFCloudMessages(temp_data, t_chart, t_options,
 		(items || [])
 		.map(({ message }) => message)
 		.slice().reverse()
-		.forEach(({ appID, TEMP, HUMID, RTT, TIME, IN, OUT }) => {
+		.forEach(({ appID, TEMP, HUMID, NAME, RTT, TIME, IN, OUT }) => {
 			if (!primaryUpdateFunc[appID]) {
 				console.log('unhandled appID', appID);
 				return;
@@ -96,12 +94,20 @@ function checkNRFCloudMessages(temp_data, t_chart, t_options,
 				case "Thingy" :
 					secondaryUpdateFunc[appID](TEMP);
 					primaryUpdateFunc[appID](HUMID);
+					if (NAME == "Hive1"){
+						temp_arr[0] = temp;
+						humid_arr[0] = humid;
+					} 
+					if (NAME == "Hive2") {
+						temp_arr[1] = temp;
+						humid_arr[1] = humid;
+					}
 					updateTime[appID](TIME);
 					// update temperature chart
-					temp_data.addRow([index, temp]);
+					temp_data.addRow([index, temp_arr[0], temp_arr[1]]);
 					t_chart.draw(temp_data, t_options);
 					// update humidity chart
-					humid_data.addRow([index, humid]);
+					humid_data.addRow([index, humid_arr[0], humid_arr[1]]);
 					h_chart.draw(humid_data, h_options);
 					break;
 				case "BM-W" :
@@ -176,18 +182,20 @@ function drawChart() {
 	var temp_data = new google.visualization.DataTable();
 	let initialDate = new Date();
 	temp_data.addColumn("datetime","Time");
-	temp_data.addColumn("number","Temperature");
+	temp_data.addColumn("number","Hive 1");
+	temp_data.addColumn("number","Hive 2");
 	temp_data.addRow([new Date(initialDate.getFullYear(),initialDate.getMonth(), 
 					 initialDate.getDate(), initialDate.getHours(), initialDate.getMinutes(), 
-					 initialDate.getSeconds()), NaN]);
+					 initialDate.getSeconds()), NaN, NaN]);
 	
 	// create humid data object with default value
 	var humid_data = new google.visualization.DataTable();
 	humid_data.addColumn("datetime","Time");
-	humid_data.addColumn("number","Humidity");
+	humid_data.addColumn("number","Hive 1");
+	humid_data.addColumn("number","Hive 2");
 	humid_data.addRow([new Date(initialDate.getFullYear(),initialDate.getMonth(), 
 					  initialDate.getDate(), initialDate.getHours(), initialDate.getMinutes(), 
-					  initialDate.getSeconds()), NaN]);
+					  initialDate.getSeconds()), NaN, NaN]);
 	
 	// create data object for weight graph
 	var weight_data = new google.visualization.DataTable();
@@ -212,11 +220,19 @@ function drawChart() {
 	  hAxis: {
 		title: "Time"
 	  },
+	  series: {
+		0: {targetAxisIndex: 0, color: "#007f00"},
+		1: {targetAxisIndex: 1, color: "#ee82ee"}
+	  },
+	  vAxes: {
+		  0: {title: 'Temperature hive 1'},
+		  1: {title: 'Temperature hive 2'}
+	  },
 	  vAxis: {
-		viewWindow: {
-		  max: 45,
-		  min: 0
-		},
+		// viewWindow: {
+		//   max: 45,
+		//   min: 0
+		// },
 		title: "Temp (celsius)"
 	  }
 	};
@@ -226,13 +242,21 @@ function drawChart() {
 	  hAxis: {
 		title: "Time"
 	  },
+	  series: {
+		0: {targetAxisIndex: 0, color: "#007f00"},
+		1: {targetAxisIndex: 1, color: "#ee82ee"}
+	  },
+	  vAxes: {
+		0: {title: 'Humidity hive 1'},
+		1: {title: 'Humidity hive 2'}
+	  },
 	  vAxis: {
-		viewWindow: {
-		  //max: 70
-		},
+		// viewWindow: {
+		//   //max: 70
+		// },
 		title: "Humidity (%)"
 	  },
-	  colors: ["#a52714"]
+	//   colors: ["#a52714"]
 	};
 	// create options for weight object with titles etc.
 	let w_options = {
@@ -258,7 +282,7 @@ function drawChart() {
 		series: {
 			0: {targetAxisIndex: 0, color: "#007f00"},
 			1: {targetAxisIndex: 1, color: "#ee82ee"}
-		  },
+		},
 		vAxes: {
 			// Adds titles to each axis.
 			0: {title: 'Number of bees in'},
